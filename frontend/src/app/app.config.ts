b9d5047d918fd6318dece52956e887b854e91cee
AbdelhadiOumar <abdelhadioumar@gmail.com>
2026-03-15 15:03:53 -0500

    chore: configure routing and global styles

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,8 +1,14 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes, withComponentInputBinding(), withViewTransitions()),
+    provideHttpClient(withInterceptors([authInterceptor])),
+  ],
 };
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
+import { guestGuard } from './core/guards/guest.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    canActivate: [guestGuard],
+    loadChildren: () => import('./features/auth/auth.routes').then((m) => m.AUTH_ROUTES),
+  },
+  {
+    path: 'rooms',
+    canActivate: [authGuard],
+    loadChildren: () => import('./features/rooms/rooms.routes').then((m) => m.ROOMS_ROUTES),
+  },
+  { path: '', redirectTo: 'rooms', pathMatch: 'full' },
+  {
+    path: '**',
+    loadComponent: () => import('./shared/components/not-found').then((m) => m.NotFoundComponent),
+  },
+];
